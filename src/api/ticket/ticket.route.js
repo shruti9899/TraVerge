c192d829e9e74178c0acf1b7bfc0b81a56cc8e5d
Shruti Shah <shrutishah.20199@gmail.com>
2020-05-27 19:43:34 +0530

    Final

--- src/api/ticket/ticket.route.js
+++ src/api/ticket/ticket.route.js
@@ -9,12 +9,12 @@ const ticketParam = require('./ticket.param')
 const router = express.Router()
 
 //routes with role based grants and parameter validation middlewares
-router.get('/status/:status',access.grantAccess('readAny','Ticket'),validate(ticketParam.viewTicketsByStatus),ticketCtrl.viewTicketsByStatus)
-router.get('/:ticketId',access.grantAccess('readAny','Ticket'),validate(ticketParam.viewStatusById),ticketCtrl.viewStatusById)
-router.get('/passenger/:ticketId',access.grantAccess('readAny','Ticket'),validate(ticketParam.viewPassengerDetails),ticketCtrl.viewPassengerDetails)
+router.get('/status/:status', access.grantAccess('readAny','Ticket'), validate(ticketParam.viewTicketsByStatus), ticketCtrl.viewTicketsByStatus)
+router.get('/:ticketId', access.grantAccess('readAny','Ticket'), validate(ticketParam.viewStatusById), ticketCtrl.viewStatusById)
+router.get('/passenger/:ticketId', access.grantAccess('readAny','Ticket'), validate(ticketParam.viewPassengerDetails), ticketCtrl.viewPassengerDetails)
 
-router.put('/update/:ticketId',access.grantAccess('updateAny','Ticket'),validate(ticketParam.updateTicket),ticketCtrl.updateTicket)
+router.put('/update/:ticketId', access.grantAccess('updateAny','Ticket'), validate(ticketParam.updateTicket), ticketCtrl.updateTicket)
 
-router.post('/reset',access.grantAccess('createAny','Ticket'),validate(ticketParam.resetAll),ticketCtrl.resetAll)
+router.post('/reset', access.grantAccess('createAny','Ticket'), validate(ticketParam.resetAll), ticketCtrl.resetAll)
 
 module.exports = router
